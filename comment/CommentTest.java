b556ac94e14837da7d331f1af502e38dab77727b
wangzhao <13132398914@163.com>
2020-07-01 23:20:18 +0800

    熟悉Java中的单行注释与多行注释

---
+++ comment/CommentTest.java
@@ -0,0 +1,16 @@
+/**
+* 熟悉Java中的单行注释与多行注释
+*/
+public class CommentTest{
+	public static void main(String[] args){
+		/*
+			多行注释：注释多行代码,被注释的代码将不会被编译执行。
+			Systetm.out.println("你好");
+			System.out.println("Java");
+		*/
+		System.out.println("Hello World!");
+		//单行代码注释，注释单行代码，被注释的单行代码将不会被编译执行
+		//System.out.println("单行注释，注释单行代码");
+	}
+
+}
