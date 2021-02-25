a2b71352927cac5278321f68973aa8fbc0032bf4
simplewz <13132398914@163.com>
2021-02-25 15:25:38 +0800

    Object类中的方法

---
+++ BasicClass/ObjectsMethod.java
@@ -0,0 +1,11 @@
+public class ObjectsMethod{
+/**
+ * 1.boolean equals(Object obj):判断指定对象与该对象是否相等。Object默认的判断标准是两个对象是同一个对象时
+ *	返回true，此处的同一个对象的概念指两个对象的内存地址相同。
+ * 2.protected void finalize()：当系统中没有引用变量引用该对象时,系统的垃圾回收期调用此方法来清理该对象的资源。
+ * 3.Class<?> getClass():返回对象运行时类。
+ * 4.int hashCode():返回该对象的hashCode值。
+ * 5.String toString():返回该对象的字符串表示。
+ * 6.wait notify notifyAll 与线程控制有关的方法
+ */
+}
